68dbc3328a31470f49be5cbe11b682c3e5523b07
WebMohit20 <2003mohitkhemani@gmail.com>
2024-10-18 11:53:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
   // your code here
+	let str=s.split(" ");
+	return str[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+// 
